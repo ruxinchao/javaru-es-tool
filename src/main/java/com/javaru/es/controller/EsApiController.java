e3d92f2e8bdcaa0f8e453c1c1fe0fee9424a8d38
ruxinchao <18300675679@163.com>
2021-12-31 19:01:07 +0800

    1

--- src/main/java/com/javaru/es/controller/EsApiController.java
+++ src/main/java/com/javaru/es/controller/EsApiController.java
@@ -4,16 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
+import com.javaru.common.annotation.Log;
 import com.javaru.common.core.controller.BaseController;
 import com.javaru.common.core.domain.AjaxResult;
+import com.javaru.common.core.text.Convert;
+import com.javaru.common.enums.BusinessType;
 import com.javaru.es.domain.EsIndex;
 import com.javaru.es.service.IEsIndexOptService;
 import com.javaru.es.service.IEsIndexService;
+import com.javaru.generator.domain.GenTable;
 
 /**
  * 索引管理Controller
@@ -47,6 +53,32 @@ public class EsApiController extends BaseController {
 		return AjaxResult.success(transformData(indexList));
 	}
 
+	/**
+	 * 导入索引（保存）
+	 */
+	@PreAuthorize("@ss.hasPermi('es:esIndex:add')")
+	@Log(title = "导入索引", businessType = BusinessType.IMPORT)
+	@PostMapping("/importIndex")
+	public AjaxResult importIndex(String indexs) {
+		String[] indexNames = Convert.toStrArray(indexs);
+		// 查询表信息
+		for (String indexName : indexNames) {
+			EsIndex esIndex = new EsIndex();
+			esIndex.setIndexName(indexName);
+			esIndex.setIndexShowName(indexName);
+			esIndex.setStatus("0");
+			esIndexService.insertEsIndex(esIndex);
+		}
+		
+		return AjaxResult.success();
+	}
+
+	/**
+	 * listString 转换成 List<JSONObject>
+	 * 
+	 * @param list
+	 * @return
+	 */
 	private List<JSONObject> transformData(List<String> list) {
 		List<JSONObject> resList = new ArrayList<JSONObject>();
 		for (String str : list) {
